35b0ebb5d9c49a358a1991b3e818dc2f0542aa7d
sanidhya kanani <91957768+Sanidhya247@users.noreply.github.com>
2022-12-15 18:14:24 +0530

    initial commit

---
+++ pages/tshirts.js
@@ -0,0 +1,105 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Tshirts = () => {
+  return (
+    <div>
+      <section className="text-gray-600 body-font container">
+  <div className="container px-5 py-24 mx-auto  ">
+    <div className="flex flex-wrap ">
+      <div className=" md:text-left lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg cursor-pointer m-9">
+        <a className="block relative  rounded overflow-hidden">
+          <Image alt="ecommerce" width={1000} height={1000} className="m-auto md:mx-0 block" src={"/tshirt.png"}/>
+        </a>
+        <div className="mt-4 text-center md:text-left">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
+          <p className="mt-1">M , L , XL , XXL , XXXL </p>
+        </div>
+      </div>
+      <div className=" md:text-left lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg cursor-pointer m-9">
+        <a className="block relative  rounded overflow-hidden">
+          <Image alt="ecommerce" width={1000} height={1000} className="m-auto md:mx-0 block" src={"/tshirt.png"}/>
+        </a>
+        <div className="mt-4 text-center md:text-left">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
+          <p className="mt-1">M , L , XL , XXL , XXXL </p>
+        </div>
+      </div>
+      <div className=" md:text-left lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg cursor-pointer m-9">
+        <a className="block relative  rounded overflow-hidden">
+          <Image alt="ecommerce" width={1000} height={1000} className="m-auto md:mx-0 block" src={"/tshirt.png"}/>
+        </a>
+        <div className="mt-4 text-center md:text-left">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
+          <p className="mt-1">M , L , XL , XXL , XXXL </p>
+        </div>
+      </div>
+      <div className=" md:text-left lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg cursor-pointer m-9">
+        <a className="block relative  rounded overflow-hidden">
+          <Image alt="ecommerce" width={1000} height={1000} className="m-auto md:mx-0 block" src={"/tshirt.png"}/>
+        </a>
+        <div className="mt-4 text-center md:text-left">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
+          <p className="mt-1">M , L , XL , XXL , XXXL </p>
+        </div>
+      </div>
+      <div className=" md:text-left lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg cursor-pointer m-9">
+        <a className="block relative  rounded overflow-hidden">
+          <Image alt="ecommerce" width={1000} height={1000} className="m-auto md:mx-0 block" src={"/tshirt.png"}/>
+        </a>
+        <div className="mt-4 text-center md:text-left">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
+          <p className="mt-1">M , L , XL , XXL , XXXL </p>
+        </div>
+      </div>
+      <div className=" md:text-left lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg cursor-pointer m-9">
+        <a className="block relative  rounded overflow-hidden">
+          <Image alt="ecommerce" width={1000} height={1000} className="m-auto md:mx-0 block" src={"/tshirt.png"}/>
+        </a>
+        <div className="mt-4 text-center md:text-left">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
+          <p className="mt-1">M , L , XL , XXL , XXXL </p>
+        </div>
+      </div>
+      <div className=" md:text-left lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg cursor-pointer m-9">
+        <a className="block relative  rounded overflow-hidden">
+          <Image alt="ecommerce" width={1000} height={1000} className="m-auto md:mx-0 block" src={"/tshirt.png"}/>
+        </a>
+        <div className="mt-4 text-center md:text-left">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
+          <p className="mt-1">M , L , XL , XXL , XXXL </p>
+        </div>
+      </div>
+      <div className=" md:text-left lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg cursor-pointer m-9">
+        <a className="block relative  rounded overflow-hidden">
+          <Image alt="ecommerce" width={1000} height={1000} className="m-auto md:mx-0 block" src={"/tshirt.png"}/>
+        </a>
+        <div className="mt-4 text-center md:text-left">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
+          <p className="mt-1">M , L , XL , XXL , XXXL </p>
+        </div>
+      </div>      
+    </div>
+  </div>
+</section>
+    </div>
+  )
+}
+
+export default Tshirts
